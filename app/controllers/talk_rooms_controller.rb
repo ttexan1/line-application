0213e689c9441983ea483e48dbe800a43e892357
ttexan <ttexan.developper@gmail.com>
2018-02-27 16:22:19 +0900

    make talking room

--- app/controllers/talk_rooms_controller.rb
+++ app/controllers/talk_rooms_controller.rb
@@ -1,7 +1,10 @@
 class TalkRoomsController < ApplicationController
   def index
     @user = current_user
+    @groups = @user.groups.includes(:talk_room)
   end
   def show
+    @talk_room = TalkRoom.find(params[:id])
+    @messages = @talk_room.messages
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   has_many :users_groups
   has_many :users, through: :users_groups
+  has_one :talk_room
+  # accepts_nested_attributes_for :group_users
+  # 他のモデルを一括で更新、保存できるようにする
+  # groupを保存するのと同時にgroup_usersを更新できる
 end
