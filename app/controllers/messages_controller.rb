f5874dd70f68afb8ec0d472a1a1b30283863d479
ttexan <ttexan.developper@gmail.com>
2018-03-02 16:28:59 +0900

    pagenation by ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    @messages = @group.messages
+    @messages = @group.messages.page(params[:page]).per(5)
     @room = @group.room(current_user)
   end
   def create
