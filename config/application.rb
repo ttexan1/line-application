ff6b0acc763481eba538016c22f36cf0483c71a0
ttexan <ttexan.developper@gmail.com>
2018-03-04 12:24:02 +0900

    time zone set up

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module LineApplication
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
